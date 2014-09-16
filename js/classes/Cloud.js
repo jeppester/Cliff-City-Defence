73cd8418d0a234765987c39188660b11af3812cf
Jesper Sørensen <jeppester1@gmail.com>
2014-09-16 09:09:47 +0200

    Clouds are now moving a more natural way

--- js/classes/Cloud.js
+++ js/classes/Cloud.js
@@ -1,16 +1,16 @@
 Cloud = function () {
-	View.GameObject.call(this, 'Backgrounds.Cloud', Math.random() * engine.canvasResX, 50 + Math.random() * 100, 0, {offset: new Math.Vector(5 + Math.random() * 10, 0)});
+	View.GameObject.call(this, 'Backgrounds.Cloud', Math.random() * engine.canvasResX, 50 + Math.random() * 100, 0);
 
 	engine.currentRoom.loops.eachFrame.attachFunction(this, this.checkOutside);
 
 	this.size *=  0.7 + Math.random() *  0.3;
-	this.speed.x = 10 + Math.random() * 5;
+	this.speed.x = -5 - Math.random() * 10;
 };
 
 Cloud.prototype = Object.create(View.GameObject.prototype);
 
 Cloud.prototype.checkOutside = function () {
-	if (this.x > engine.canvasResX + this.bm.width / 2) {
-		this.x = 0;
+	if (this.x < -this.bm.width / 2) {
+		this.x = engine.canvasResX + this.bm.width / 2;
 	}
 };
